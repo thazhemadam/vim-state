192fc96478f4fdfdfc6cc1d1b732d7d5214a2330
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2026-07-02 11:22:12 +0530

    revert: revert "fix(pi): make Insert-mode Enter insert a newline"
    This reverts commit a57487ab81b06cecf717eab54f28e8d8fe2ebaca.
    
    Let's be consistent with how other TUIs do this and allow this behavior,
    as it is expected. To insert a new line, we must Shift+Enter instead.

--- src/adapters/pi/editor.ts
+++ src/adapters/pi/editor.ts
@@ -75,7 +75,7 @@ export class VimPiEditor extends VimEditor(PiEditorHost) {
     // If you were in Insert mode and are still in Insert mode,
     // then pass the data to the underlying Pi editor.
     if (previousMode === "insert" && mode === "insert") {
-      super.handleInput(event.key === "enter" ? "\n" : data);
+      super.handleInput(data);
     } else if (
       previousMode === "replace" &&
       mode === "replace" &&
